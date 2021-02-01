a147e36185e439e63e27b99c12df6622ce62e4d7
Rafał Podkoński <rafal.podkonski@dev-software.pl>
2021-02-01 08:38:56 +0100

    First code in TypeScript

---
+++ 1/projekt1.ts
@@ -0,0 +1,2 @@
+const username = window.prompt('What is your name?');
+window.alert(`Hello ${username}`);
---
+++ 1/test.ts
@@ -0,0 +1,8 @@
+function add(a: number, b: number) {
+  return a + b;
+}
+
+console.log(add(1, 2));
+console.log(add('1', '2'));
+console.log(add({}, []));
+console.log(add(null, undefined));
